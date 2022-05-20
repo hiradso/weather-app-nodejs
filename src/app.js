e1071fa7214949a0dec01001c9a4cff8081d686e
hiradso <hiradso@caveman.caveman>
2022-05-20 20:01:37 +0430

    some modification

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const hbs = require("hbs");
 const geocode = require("./utils/geocode");
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
